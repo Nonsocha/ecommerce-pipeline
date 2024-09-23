f793ba0def7e094c9ced8d0c50e033f809d422ef
Nonsocha <162307332+Nonsocha@users.noreply.github.com>
2024-09-23 12:56:57 +0100

    Create package.js

---
+++ package.js
@@ -0,0 +1,17 @@
+{
+  "name": "ecommerce-app",
+  "version": "1.0.0",
+  "description": "A simple Node.js e-commerce app",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js",
+    "dev": "nodemon server.js"
+  },
+  "dependencies": {
+    "express": "^4.17.1",
+    "mongoose": "^5.11.15"
+  },
+  "devDependencies": {
+    "nodemon": "^2.0.7"
+  }
+}
